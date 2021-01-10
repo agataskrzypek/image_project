3e529bbf7e762c07f87b5eb7361a2e8c5e09c86b
agataskrzypek <agataskrzypek1@gmail,.com>
2021-01-10 13:05:07 +0100

    develop_v4 merge request check - v2

--- App/build_app.py
+++ App/build_app.py
@@ -15,7 +15,6 @@ kivy.require('2.0.0') # replace with your current kivy version !
 #     MyApp().run()
 #     pass
 
-from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
@@ -58,6 +57,6 @@ class TestApp(App):
 
 
 if __name__ == '__main__':
-    TestApp().run().siema.
+    TestApp().siema()
 
 pass
